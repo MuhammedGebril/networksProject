6a0929417763354d602acb5bd9d9adda2c9a0959
Muhammed Gebril <moahmed.gebril1220@gmail.com>
2023-04-30 06:30:14 +0300

    added http file and updated readme

---
+++ http.py
@@ -0,0 +1,7 @@
+'''
+Application layer class
+this file will be http class , the object to be sent to the transport layer to be sent using reliable modified UDP
+'''
+import socket
+class http:
+
---
+++ segment.py
@@ -0,0 +1,11 @@
+'''
+Transport layer class
+this file will be the segment class
+In this class we should implement reliable data transfer logic
+this class should operate on an http object
+'''
+import socket
+import http
+class segment:
+
+
